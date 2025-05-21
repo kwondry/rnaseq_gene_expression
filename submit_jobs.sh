56403b76f356642051995e9483c2b7615094bbbe
Joseph Elsherbini <jelsherbini@Josephs-MacBook-Pro.local>
2025-05-21 10:36:07 -0400

    change submission script

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -8,4 +8,4 @@
 #SBATCH --job-name=submit_jobs
 
 
-snakemake  --executor slurm --default-resources slurm_partition=short --resources active_nextflow=2 --configfile config.yaml --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
+snakemake  --executor slurm --default-resources slurm_partition=short --resources active_nextflow=2 --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
