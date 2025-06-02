bbbaba9bfd6b562aafe6585ec41cd6ee5cd38737
Elsherbini, Joseph Ahmed <jelsherbini@mgh.harvard.edu>
2025-06-01 21:44:01 -0400

    add kraken grouping and nextflow retries

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,10 +2,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=5-00:00:00
-#SBATCH --mem=16GB
-#SBATCH --partition=medium
+#SBATCH --time=10-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --partition=priority
 #SBATCH --job-name=submit_jobs
 
 
-snakemake  --executor slurm --default-resources slurm_partition=short --resources active_nextflow=2 --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
+snakemake  --executor slurm --default-resources slurm_partition=short --groups kraken=kraken_group --group-components kraken_group=6 --resources active_nextflow=2 --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
