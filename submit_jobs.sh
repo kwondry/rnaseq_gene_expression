dea917c1aa0f79f73224ac069e5716f5f9957fa3
Joseph Elsherbini <jelsherbini@Josephs-MacBook-Pro.local>
2025-08-18 20:24:19 -0400

    update snakeefile to do kraken in serial and submit nextflow as individual jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -8,4 +8,9 @@
 #SBATCH --job-name=submit_jobs
 
 
-snakemake  --executor slurm --default-resources slurm_partition=short --groups kraken=kraken_group --group-components kraken_group=6 --resources active_nextflow=2 --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
+snakemake  --executor slurm --default-resources slurm_partition=short \
+  --groups kraken=kraken_group --group-components kraken_group=12 \
+  --set-threads kraken=8 \
+  --set-resources kraken:mem_mb=100000 \
+  --resources active_nextflow=1 serial_slots=1 \
+  --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
