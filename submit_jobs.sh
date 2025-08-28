3ebdbd687fc6165d3bfc819e66475fcc981c0381
Elsherbini, Joseph Ahmed <jelsherbini@mgh.harvard.edu>
2025-08-28 13:48:53 -0400

    remove kraken filtering for mouse rnaseq runs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,15 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=10-00:00:00
-#SBATCH --mem=32GB
-#SBATCH --partition=priority
+#SBATCH --time=12:00:00
+#SBATCH --mem=8GB
+#SBATCH --partition=short
 #SBATCH --job-name=submit_jobs
 
 
 snakemake  --executor slurm --default-resources slurm_partition=short \
-  --groups kraken=kraken_group --group-components kraken_group=12 \
-  --set-threads kraken=8 \
-  --set-resources kraken:mem_mb=100000 \
-  --resources active_nextflow=1 serial_slots=1 \
+  --resources active_nextflow=1\
   --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
